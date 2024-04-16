39a98b449d91c6626c0f7c4f30459a582c1bc37e
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-04-16 12:46:54 +0200

    data

--- data.js
+++ data.js
@@ -1,1 +1,16 @@
-const text = `Mohácsi csata;Mikor volt a mohácsi csata;1526;kepek;`
+const text = `Nemzeti szocializmus; Mikor robbant ki a polgári demokratikus forradalom Németországban? ;1918;
+"Nemzeti szocializmus; Mikor próbálják a kommunisták átvenni a hatalmat Berlinben?; 1919;"
+"Nemzeti szocializmus; Mikor volt a Weimari gyűlés?; 1919-1933;"
+"Nemzeti szocializmus; Mikor verik le a müncheni sörpuccsot?; 1923;"
+"Nemzeti szocializmus; Mikor kezsődött a Nácizmus?; 1933;"
+"Nemzeti szocializmus; Mikor volt népszavazás a Saar vidéken?; 1934;"
+"Nemzeti szocializmus; Mikortól része Németországnak a Saar vidék?; 1935;"
+"Nemzeti szocializmus; Mikor volt az Anschluss?; 1938;"
+"Nemzeti szocializmus; Mikor kötötték meg a müncheni egyezményt?; 1938. szept. 29-30."
+"Nemzeti szocializmus; Mikor jött rá Anglia és Franciaország Hitler igazi céljára?; 1939. márc. 15."
+"Nemzeti szocializmus; Mikor alakul meg a jobboldali pártok szövetsége (Harzburgi Front)?; 1931;"
+"Nemzeti szocializmus; Mikor nyeri meg Hitler pártja a választást?; 1932;"
+"Nemzeti szocializmus; Mikor lesz Hitler birodalmi kancellár?; 1933. jan. 30."
+"Nemzeti szocializmus; Mikor volt a hosszúkések éjszakája?; 1934. jún. 19-30."
+"Nemzeti szocializmus; Mikor hal meg Hindenburg és lesz Hitler vezér és kancellár?; 1934;"
+`
