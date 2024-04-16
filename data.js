55cc1cff4abaf94a3c658544a81c15e056f0fef3
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-04-16 12:01:00 +0200

    created data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = `Magyarország a kora újkorban; Mikor halt meg Mátyás király?; 1490;
+Magyarország a kora újkorban; Mettől meddig volt a trónon II. Ulászló?; 1490-1516;
+Magyarország a kora újkorban; Mikor volt a Rákosi országgyűlés?; 1505;
+Magyarország a kora újkorban; Mikor volt parasztfelkelés II. Ulászló uralkodása alatt?; 1514;
+Magyarország a kora újkorban; Mikor volt a Mohácsi csata?; 1526.08.29.;
+Magyarország a kora újkorban; Mikor bukott el a Nándorfehérvári vár?; 1521;
+Magyarország a kora újkorban; Mettől meddig uralkodott I. János?; 1526-1540;
+Magyarország a kora újkorban; Mikor uralkodott Habsburg Ferdinánd?; 1526-1564;
+Magyarország a kora újkorban; Mikor foglalta el I. Szulejmán Buda várát?; 1529;
+Magyarország a kora újkorban; Mikor vonult a török Bécs felé, de meg lettek állítva?; 1532;
+Magyarország a kora újkorban; Mikor kötötték meg a Váradi békét?; 1538;
+`
