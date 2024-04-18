ee0f6889987875bf1252083fbea895f77e40610c
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-04-18 09:48:05 +0200

    changed questions

--- data.js
+++ data.js
@@ -1,38 +1,38 @@
-const text = `Nemzeti szocializmus; Mikor robbant ki a polgári demokratikus forradalom Németországban?; 1918; images/nemzetszocializmus1.jfif
-Nemzeti szocializmus; Mikor próbálják a kommunisták átvenni a hatalmat Berlinben?; 1919; images/nemzetszocializmus2.png
-Nemzeti szocializmus; Mikor volt a Weimari gyűlés?; 1919-1933; images/nemzetszocializmus3.jfif
-Nemzeti szocializmus; Mikor verik le a müncheni sörpuccsot?; 1923; images/nemzetszocializmus4.jfif
-Nemzeti szocializmus; Mikor kezdődött a Nácizmus?; 1933; images/nemzetszocializmus5.jfif
-Nemzeti szocializmus; Mikor volt népszavazás a Saar vidéken?; 1934; images/nemzetszocializmus6.png
-Nemzeti szocializmus; Mikortól része Németországnak a Saar vidék?; 1935; images/nemzetszocializmus7.png
-Nemzeti szocializmus; Mikor volt az Anschluss?; 1938; images/nemzetszocializmus8.jpg
-Nemzeti szocializmus; Mikor kötötték meg a müncheni egyezményt?; 1938. szept. 29-30.; images/nemzetszocializmus9.jfif
-Nemzeti szocializmus; Mikor jött rá Anglia és Franciaország Hitler igazi céljára?; 1939. márc. 15.; images/nemzetszocializmus10.jpg
-Nemzeti szocializmus; Mikor alakul meg a jobboldali pártok szövetsége (Harzburgi Front)?; 1931; images/nemzetszocializmus11.jpg
-Nemzeti szocializmus; Mikor nyeri meg Hitler pártja a választást?; 1932; images/nemzetszocializmus12.jpeg
-Nemzeti szocializmus; Mikor lesz Hitler birodalmi kancellár?; 1933. jan. 30.; images/nemzetszocializmus13.jpg
-Nemzeti szocializmus; Mikor volt a hosszúkések éjszakája?; 1934. jún. 19-30.; images/nemzetszocializmus14.jfif
-Nemzeti szocializmus; Mikor hal meg Hindenburg és lesz Hitler vezér és kancellár?; 1934; images/nemzetszocializmus15.jpg
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor volt a Szovjet megszállás?; 1944; images/kommunistadiktatura1.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor alapították meg a Magyar Függetlenségi Forintot?; 1944. dec. 2.; images/kommunistadiktatura2.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor adták ki a földtörvényt?; 1945; images/kommunistadiktatura3.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor volt az utolsó demokratikus választás 1990-ig?; 1945; images/kommunistadiktatura4.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor lett köztársasági elnök Tildy Zoltán?; 1946; images/kommunistadiktatura5.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor lett miniszterelnök Nagy Ferenc?; 1946; images/kommunistadiktatura6.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor volt a kékcédulás választás?; 1947; images/kommunistadiktatura7.jpg
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor egyesítették az MKP-t és az MSzDP-t és jött létre a Magyar Dolgozók Pártja?; 1948; images/kommunistadiktatura8.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; Mikor jött létre a Magyar Függetlenségi Népfront?; 1949; images/kommunistadiktatura9.jpg
-Hunyadiak kora; Mettől meddig uralkodott Habsburg Albert?; 1437-1439; images/hunyadiak1.jpg
-Hunyadiak kora; Mettől meddig uralkodott I. Ulászló?; 1440-1444; images/hunyadiak2.jfif
-Hunyadiak kora; Mikor győzte le Hunyadi János Mezid bég hadseregét Erdélyben?; 1442; images/hunyadiak3.jpg
-Hunyadiak kora; Mikor volt a téli hosszú hadjárat?; 1443-1444; images/hunyadiak4.jpg
-Hunyadiak kora; Mikor ismerték el a kiskorú V. László uralmát?; 1444; images/hunyadiak5.jpg
-Hunyadiak kora; Mikor nevezték ki kormányzónak Hunyadi Jánost?; 1446; images/hunyadiak6.jpg
-Hunyadiak kora; Mikor volt a 2. rigómezei csata?; 1448; images/hunyadiak7.jpg
-Hunyadiak kora; Mikor győztunk a Nándorfehérvári ostromban?; 1456. júl. 21.; images/hunyadiak8.jpg
-Hunyadiak kora; Mikor végezték ki Hunyadi Lászlót?; 1457; images/hunyadiak9.jpg
-Hunyadiak kora; Mettől meddig uralkodott Hunyadi Mátyás?; 1458-1490; images/hunyadiak10.jpg
-Hunyadiak kora; Mikor vette be Mátyás Jajcát?; 1463; images/hunyadiak11.jfif
-Hunyadiak kora; Mikor foglalta el Mátyás Szabácsot?; 1476; images/hunyadiak12.jpg
-Hunyadiak kora; Mikor győzte le Kinizsi Pál a betörő törököt Kenyérmezőnél?; 1479; images/hunyadiak13.jfif
-Hunyadiak kora; Mikor foglalta el Mátyás Bécset?; 1485; images/hunyadiak14.jfif`
+const text = `Nemzeti szocializmus; Polgári demokratikus forradalom Németországban; 1918; images/nemzetszocializmus1.jfif
+Nemzeti szocializmus; Kommunista hatalomátvétel Berlinben; 1919; images/nemzetszocializmus2.png
+Nemzeti szocializmus; Weimari gyűlés; 1919-1933; images/nemzetszocializmus3.jfif
+Nemzeti szocializmus; Müncheni sörpuccs leverése; 1923; images/nemzetszocializmus4.jfif
+Nemzeti szocializmus; Nácizmus kezdete; 1933; images/nemzetszocializmus5.jfif
+Nemzeti szocializmus; Saar vidéki népszavazás; 1934; images/nemzetszocializmus6.png
+Nemzeti szocializmus; Saar vidék Németország része lesz; 1935; images/nemzetszocializmus7.png
+Nemzeti szocializmus; Anschlussz; 1938; images/nemzetszocializmus8.jpg
+Nemzeti szocializmus; Müncheni egyezmény; 1938. szept. 29-30.; images/nemzetszocializmus9.jfif
+Nemzeti szocializmus; Hitler igazi célja; 1939. márc. 15.; images/nemzetszocializmus10.jpg
+Nemzeti szocializmus; Jobboldali pártok szövetsége (Harzburgi Front); 1931; images/nemzetszocializmus11.jpg
+Nemzeti szocializmus; Hitler pártja megnyeri a választást; 1932; images/nemzetszocializmus12.jpeg
+Nemzeti szocializmus; Hitler birodalmi kancellár; 1933. jan. 30.; images/nemzetszocializmus13.jpg
+Nemzeti szocializmus; Hosszúkések éjszakája; 1934. jún. 19-30.; images/nemzetszocializmus14.jfif
+Nemzeti szocializmus; Hindenburg halála; 1934; images/nemzetszocializmus15.jpg
+A kommunista diktatúra kiépítése és működése Magyarországon; Szovjet megszállás; 1944; images/kommunistadiktatura1.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; Magyar Függetlenségi Forint; 1944. dec. 2.; images/kommunistadiktatura2.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; Földtörvényt; 1945; images/kommunistadiktatura3.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; Utolsó demokratikus választás; 1945; images/kommunistadiktatura4.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; Tildy Zoltán köztársasági elnök; 1946; images/kommunistadiktatura5.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; Nagy Ferenc miniszterelnök; 1946; images/kommunistadiktatura6.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; Kékcédulás választás; 1947; images/kommunistadiktatura7.jpg
+A kommunista diktatúra kiépítése és működése Magyarországon; MKP és MSzDP egyesítése; 1948; images/kommunistadiktatura8.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; Magyar Függetlenségi Népfront; 1949; images/kommunistadiktatura9.jpg
+Hunyadiak kora; Habsburg Albert uralkodása; 1437-1439; images/hunyadiak1.jpg
+Hunyadiak kora; I. Ulászló uralkodása; 1440-1444; images/hunyadiak2.jfif
+Hunyadiak kora; Hunyadi János legyőzi Mezid bég hadseregét Erdélyben; 1442; images/hunyadiak3.jpg
+Hunyadiak kora; Téli hosszú hadjárat; 1443-1444; images/hunyadiak4.jpg
+Hunyadiak kora; A kiskorú V. László trónra kerül; 1444; images/hunyadiak5.jpg
+Hunyadiak kora; Hunyadi Jánost kormánzó; 1446; images/hunyadiak6.jpg
+Hunyadiak kora; 2. rigómezei csata; 1448; images/hunyadiak7.jpg
+Hunyadiak kora; Nándorfehérvári ostrom győzelem; 1456. júl. 21.; images/hunyadiak8.jpg
+Hunyadiak kora; Hunyadi Lászlót kivégzése; 1457; images/hunyadiak9.jpg
+Hunyadiak kora; Hunyadi Mátyás uralkodása; 1458-1490; images/hunyadiak10.jpg
+Hunyadiak kora; Jajca bevétele; 1463; images/hunyadiak11.jfif
+Hunyadiak kora; Szabács elfoglalása; 1476; images/hunyadiak12.jpg
+Hunyadiak kora; Kinizsi Pál legyőzi a törököt Kenyérmezőnél; 1479; images/hunyadiak13.jfif
+Hunyadiak kora; Bécs elfoglalása; 1485; images/hunyadiak14.jfif`
