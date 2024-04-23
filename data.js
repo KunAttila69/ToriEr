3a20a7b06a9fa75ca4e7dfae9ac90366ae7b5aa3
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-04-23 12:35:29 +0200

    more pics

--- data.js
+++ data.js
@@ -1,38 +1,38 @@
-const text = `Nemzeti szocializmus; [11] Polgári demokratikus forradalom Németországban; 1918; images/nemzetszocializmus1.jfif; images/nemzetszocializmus16.jfif
-Nemzeti szocializmus; [11] Kommunista hatalomátvétel Berlinben; 1919; images/nemzetszocializmus2.png; images/nemzetszocializmus17.jpg
-Nemzeti szocializmus; [11] Weimari gyűlés; 1919-1933; images/nemzetszocializmus3.jfif; images/nemzetszocializmus18.jfif
-Nemzeti szocializmus; [11] Müncheni sörpuccs leverése; 1923; images/nemzetszocializmus4.jfif; images/nemzetszocializmus19.jpg
-Nemzeti szocializmus; [11] Nácizmus kezdete; 1933; images/nemzetszocializmus5.jfif; images/nemzetszocializmus20.jpg
-Nemzeti szocializmus; [11] Saar vidéki népszavazás; 1934; images/nemzetszocializmus6.png; images/nemzetszocializmus21.jpg
-Nemzeti szocializmus; [11] Saar vidék Németország része lesz; 1935; images/nemzetszocializmus7.png; images/nemzetszocializmus22.jfif
-Nemzeti szocializmus; [11] Anschlussz; 1938; images/nemzetszocializmus8.jpg; images/nemzetszocializmus23.png
-Nemzeti szocializmus; [11] Müncheni egyezmény; 1938. szept. 29-30.; images/nemzetszocializmus9.jfif; images/nemzetszocializmus24.jfif
-Nemzeti szocializmus; [11] Hitler igazi célja; 1939. márc. 15.; images/nemzetszocializmus10.jpg; images/nemzetszocializmus25.jfif
-Nemzeti szocializmus; [11] Jobboldali pártok szövetsége (Harzburgi Front); 1931; images/nemzetszocializmus11.jpg; images/nemzetszocializmus26.jfif
-Nemzeti szocializmus; [11] Hitler pártja megnyeri a választást; 1932; images/nemzetszocializmus12.jpeg; images/nemzetszocializmus27.jpg
-Nemzeti szocializmus; [11] Hitler birodalmi kancellár; 1933. jan. 30.; images/nemzetszocializmus13.jpg; images/nemzetszocializmus28.jpg
-Nemzeti szocializmus; [11] Hosszúkések éjszakája; 1934. jún. 19-30.; images/nemzetszocializmus14.jfif; images/nemzetszocializmus29.jfif
-Nemzeti szocializmus; [11] Hindenburg halála; 1934; images/nemzetszocializmus15.jpg; images/nemzetszocializmus30.jpg
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Szovjet megszállás; 1944; images/kommunistadiktatura1.jfif; images/kommunistadiktatura10.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Magyar Függetlenségi Forint; 1944. dec. 2.; images/kommunistadiktatura2.jfif; images/kommunistadiktatura11.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Földtörvény; 1945; images/kommunistadiktatura3.jfif; images/kommunistadiktatura12.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Utolsó demokratikus választás; 1945; images/kommunistadiktatura4.jfif; images/kommunistadiktatura13.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Tildy Zoltán köztársasági elnök; 1946; images/kommunistadiktatura5.jfif; images/kommunistadiktatura14.jpg
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Nagy Ferenc miniszterelnök; 1946; images/kommunistadiktatura6.jfif; images/kommunistadiktatura15.jpg
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Kékcédulás választás; 1947; images/kommunistadiktatura7.jpg; images/kommunistadiktatura16.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] MKP és MSzDP egyesítése; 1948; images/kommunistadiktatura8.jfif; images/kommunistadiktatura17.jfif
-A kommunista diktatúra kiépítése és működése Magyarországon; [13] Magyar Függetlenségi Népfront; 1949; images/kommunistadiktatura9.jpg; images/kommunistadiktatura18.jfif
-Hunyadiak kora; [16] Habsburg Albert uralkodása; 1437-1439; images/hunyadiak1.jpg; images/hunyadiak15.jfif
-Hunyadiak kora; [16] I. Ulászló uralkodása; 1440-1444; images/hunyadiak2.jfif; images/hunyadiak16.jfif
-Hunyadiak kora; [16] Hunyadi János legyőzi Mezid bég hadseregét Erdélyben; 1442; images/hunyadiak3.jpg; images/hunyadiak17.jpg
-Hunyadiak kora; [16] Téli hosszú hadjárat; 1443-1444; images/hunyadiak4.jpg; images/hunyadiak18.jpg
-Hunyadiak kora; [16] A kiskorú V. László trónra kerül; 1444; images/hunyadiak5.jpg; images/hunyadiak19.jfif
-Hunyadiak kora; [16] Hunyadi Jánost kormányzó; 1446; images/hunyadiak6.jpg; images/hunyadiak20.jfif
-Hunyadiak kora; [16] 2. rigómezei csata; 1448; images/hunyadiak7.jpg; images/hunyadiak21.png
-Hunyadiak kora; [16] Nándorfehérvári ostrom győzelem; 1456. júl. 21.; images/hunyadiak8.jpg; images/hunyadiak22.jpg
-Hunyadiak kora; [16] Hunyadi Lászlót kivégzése; 1457; images/hunyadiak9.jpg; images/hunyadiak23.jfif
-Hunyadiak kora; [16] Hunyadi Mátyás uralkodása; 1458-1490; images/hunyadiak10.jpg; images/hunyadiak24.jpg
-Hunyadiak kora; [16] Jajca bevétele; 1463; images/hunyadiak11.jfif; images/hunyadiak25.jfif
-Hunyadiak kora; [16] Szabács elfoglalása; 1476; images/hunyadiak12.jpg; images/hunyadiak26.jpg
-Hunyadiak kora; [16] Kinizsi Pál legyőzi a törököt Kenyérmezőnél; 1479; images/hunyadiak13.jfif; images/hunyadiak27.jpg
-Hunyadiak kora; [16] Bécs elfoglalása; 1485; images/hunyadiak14.jfif; images/hunyadiak28.jfif`
+const text = `Nemzeti szocializmus; [11] Polgári demokratikus forradalom Németországban; 1918; images/nemzetszocializmus1.jfif; images/nemzetszocializmus16.jfif; images/nemzetszocializmus45.jfif
+Nemzeti szocializmus; [11] Kommunista hatalomátvétel Berlinben; 1919; images/nemzetszocializmus2.png; images/nemzetszocializmus17.jpg; images/nemzetszocializmus44.jfif
+Nemzeti szocializmus; [11] Weimari gyűlés; 1919-1933; images/nemzetszocializmus3.jfif; images/nemzetszocializmus18.jfif; images/nemzetszocializmus43.jfif
+Nemzeti szocializmus; [11] Müncheni sörpuccs leverése; 1923; images/nemzetszocializmus4.jfif; images/nemzetszocializmus19.jpg; images/nemzetszocializmus42.jfif
+Nemzeti szocializmus; [11] Nácizmus kezdete; 1933; images/nemzetszocializmus5.jfif; images/nemzetszocializmus20.jpg; images/nemzetszocializmus41.jfif
+Nemzeti szocializmus; [11] Saar vidéki népszavazás; 1934; images/nemzetszocializmus6.png; images/nemzetszocializmus21.jpg; images/nemzetszocializmus40.jfif
+Nemzeti szocializmus; [11] Saar vidék Németország része lesz; 1935; images/nemzetszocializmus7.png; images/nemzetszocializmus22.jfif; images/nemzetszocializmus39.jfif
+Nemzeti szocializmus; [11] Anschlussz; 1938; images/nemzetszocializmus8.jpg; images/nemzetszocializmus23.png; images/nemzetszocializmus38.jfif
+Nemzeti szocializmus; [11] Müncheni egyezmény; 1938. szept. 29-30.; images/nemzetszocializmus9.jfif; images/nemzetszocializmus24.jfif; images/nemzetszocializmus37.jfif
+Nemzeti szocializmus; [11] Hitler igazi célja; 1939. márc. 15.; images/nemzetszocializmus10.jpg; images/nemzetszocializmus25.jfif; images/nemzetszocializmus36.jfif
+Nemzeti szocializmus; [11] Jobboldali pártok szövetsége (Harzburgi Front); 1931; images/nemzetszocializmus11.jpg; images/nemzetszocializmus26.jfif; images/nemzetszocializmus35.png
+Nemzeti szocializmus; [11] Hitler pártja megnyeri a választást; 1932; images/nemzetszocializmus12.jpeg; images/nemzetszocializmus27.jpg; images/nemzetszocializmus34.jpg
+Nemzeti szocializmus; [11] Hitler birodalmi kancellár; 1933. jan. 30.; images/nemzetszocializmus13.jpg; images/nemzetszocializmus28.jpg; images/nemzetszocializmus33.jpg
+Nemzeti szocializmus; [11] Hosszúkések éjszakája; 1934. jún. 19-30.; images/nemzetszocializmus14.jfif; images/nemzetszocializmus29.jfif; images/nemzetszocializmus32.jpg
+Nemzeti szocializmus; [11] Hindenburg halála; 1934; images/nemzetszocializmus15.jpg; images/nemzetszocializmus30.jpg; images/nemzetszocializmus31.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Szovjet megszállás; 1944; images/kommunistadiktatura1.jfif; images/kommunistadiktatura10.jfif; images/kommunistadiktatura27.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Magyar Függetlenségi Forint; 1944. dec. 2.; images/kommunistadiktatura2.jfif; images/kommunistadiktatura11.jfif; images/kommunistadiktatura26.jpg
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Földtörvény; 1945; images/kommunistadiktatura3.jfif; images/kommunistadiktatura12.jfif; images/kommunistadiktatura25.jpg
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Utolsó demokratikus választás; 1945; images/kommunistadiktatura4.jfif; images/kommunistadiktatura13.jfif; images/kommunistadiktatura24.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Tildy Zoltán köztársasági elnök; 1946; images/kommunistadiktatura5.jfif; images/kommunistadiktatura14.jpg; images/kommunistadiktatura23.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Nagy Ferenc miniszterelnök; 1946; images/kommunistadiktatura6.jfif; images/kommunistadiktatura15.jpg; images/kommunistadiktatura22.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Kékcédulás választás; 1947; images/kommunistadiktatura7.jpg; images/kommunistadiktatura16.jfif; images/kommunistadiktatura21.jfif
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] MKP és MSzDP egyesítése; 1948; images/kommunistadiktatura8.jfif; images/kommunistadiktatura17.jfif; images/kommunistadiktatura20.jpg
+A kommunista diktatúra kiépítése és működése Magyarországon; [13] Magyar Függetlenségi Népfront; 1949; images/kommunistadiktatura9.jpg; images/kommunistadiktatura18.jfif; images/kommunistadiktatura19.jfif
+Hunyadiak kora; [16] Habsburg Albert uralkodása; 1437-1439; images/hunyadiak1.jpg; images/hunyadiak15.jfif; images/hunyadiak42.jpg
+Hunyadiak kora; [16] I. Ulászló uralkodása; 1440-1444; images/hunyadiak2.jfif; images/hunyadiak16.jfif; images/hunyadiak41.jfif
+Hunyadiak kora; [16] Hunyadi János legyőzi Mezid bég hadseregét Erdélyben; 1442; images/hunyadiak3.jpg; images/hunyadiak17.jpg; images/hunyadiak40.jfif
+Hunyadiak kora; [16] Téli hosszú hadjárat; 1443-1444; images/hunyadiak4.jpg; images/hunyadiak18.jpg; images/hunyadiak39.jfif
+Hunyadiak kora; [16] A kiskorú V. László trónra kerül; 1444; images/hunyadiak5.jpg; images/hunyadiak19.jfif; images/hunyadiak38.jpg
+Hunyadiak kora; [16] Hunyadi Jánost kormányzó; 1446; images/hunyadiak6.jpg; images/hunyadiak20.jfif; images/hunyadiak37.jfif
+Hunyadiak kora; [16] 2. rigómezei csata; 1448; images/hunyadiak7.jpg; images/hunyadiak21.png; images/hunyadiak36.jfif
+Hunyadiak kora; [16] Nándorfehérvári ostrom győzelem; 1456. júl. 21.; images/hunyadiak8.jpg; images/hunyadiak22.jpg; images/hunyadiak35.jfif
+Hunyadiak kora; [16] Hunyadi Lászlót kivégzése; 1457; images/hunyadiak9.jpg; images/hunyadiak23.jfif; images/hunyadiak34.jfif
+Hunyadiak kora; [16] Hunyadi Mátyás uralkodása; 1458-1490; images/hunyadiak10.jpg; images/hunyadiak24.jpg; images/hunyadiak33.jfif
+Hunyadiak kora; [16] Jajca bevétele; 1463; images/hunyadiak11.jfif; images/hunyadiak25.jfif; images/hunyadiak32.jfif
+Hunyadiak kora; [16] Szabács elfoglalása; 1476; images/hunyadiak12.jpg; images/hunyadiak26.jpg; images/hunyadiak31.jfif
+Hunyadiak kora; [16] Kinizsi Pál legyőzi a törököt Kenyérmezőnél; 1479; images/hunyadiak13.jfif; images/hunyadiak27.jpg; images/hunyadiak30.jpg
+Hunyadiak kora; [16] Bécs elfoglalása; 1485; images/hunyadiak14.jfif; images/hunyadiak28.jfif; images/hunyadiak29.jfif`
